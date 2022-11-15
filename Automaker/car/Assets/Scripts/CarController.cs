29e22e425de26ffe31c63fc11aaa2971b5f9fb5e
Pxavier12 <pxavier12@hotmail.fr>
2022-11-15 20:28:40 +0100

    Added Radar + TextMeshPro

--- Automaker/car/Assets/Scripts/CarController.cs
+++ Automaker/car/Assets/Scripts/CarController.cs
@@ -3,16 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+    public enum SpeedUnit
+{
+    mph,
+    kmh
+}
+
 public class CarController : MonoBehaviour
 {
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
+
+    public GameObject speedTextObject;
 
     private float horizontalInput;
     private float verticalInput;
     private float currentSteerAngle;
     private float currentbreakForce;
     private bool isBreaking;
+
+    private float currentSpeed;
+ 
+    private SpeedUnit speedUnit = SpeedUnit.kmh;
+    private int decimalPlaces;
+    Rigidbody rb;
+
+
+
+
+    private double speed;
+
+
 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
@@ -82,5 +104,42 @@ public class CarController : MonoBehaviour
 ;       wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("radar"))
+        {
+            other.gameObject.SetActive(false);
+            if(currentSpeed>40){
+                speedTextObject.SetActive(true);
+            }
+        }
+    }
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        speedTextObject.SetActive(false);
+    }
+
+    void Update(){
+
+         if (speedUnit == SpeedUnit.mph)
+        {
+            // 2.23694 is the constant to convert a value from m/s to mph.
+            currentSpeed = (float)Math.Round(rb.velocity.magnitude * 2.23694f, decimalPlaces);
+        }
+
+        else 
+        {
+            // 3.6 is the constant to convert a value from m/s to km/h.
+            currentSpeed = (float)Math.Round(rb.velocity.magnitude * 3.6f, decimalPlaces);
+        }
+    }
+
+    IEnumerator CalculateSpeed(){
+        Vector3 lastPosition = transform.position;
+        yield return new WaitForFixedUpdate();
+        speed = (lastPosition - transform.position).magnitude * 2.237*Time.deltaTime;
+
     }
 }
